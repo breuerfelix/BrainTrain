f5d2419e6e49c674d72013ebd4b9bec0f65e8d08
Felix Breuer <f.breuer94@gmail.com>
2019-05-02 23:28:46 +0200

    move old stuff

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -10,6 +10,7 @@ import (
 func Init() {
 	http.HandleFunc("/", controller.HandleWithContext(controller.Index))
 	http.HandleFunc("/public-registers", controller.HandleWithContext(controller.PublicRegisters))
+	http.HandleFunc("/private-registers", controller.HandleWithContext(controller.PrivateRegisters))
 	http.HandleFunc("/signup", controller.HandleWithContext(controller.Signup))
 
 	fs := http.FileServer(http.Dir("static/"))
