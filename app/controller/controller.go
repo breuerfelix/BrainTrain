a511fa0acdcb60fe39cf5239d44b735ac8d91e60
felixbreuer <i@felixbreuer.me>
2019-05-02 23:28:46 +0200

    move old stuff

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -14,6 +14,12 @@ func PublicRegisters(r *http.Request, data *pageData) {
 	data.Filename = "public-registers"
 }
 
+// PrivateRegisters controller
+func PrivateRegisters(r *http.Request, data *pageData) {
+	data.Filename = "private-registers"
+}
+
+// Signup controller
 func Signup(r *http.Request, data *pageData) {
 	data.Filename = "signup"
 }
