d0905fb0f98a3194cfc54858cc1270ef9c835293
Silvan Büdenbender <s.buedenbender@gipfelstuermer-kommunikation.com>
2019-05-05 13:33:13 +0200

    added categories as variables

--- app/controller/layout.go
+++ app/controller/layout.go
@@ -7,6 +7,11 @@ import (
 	"net/http"
 )
 
+type Categorie struct {
+	Name          string
+	SubCategories []string
+}
+
 // User struct
 type User struct {
 	NumCards     int
@@ -16,6 +21,7 @@ type User struct {
 }
 
 type pageData struct {
+	User
 	Filename            string
 	LoggedIn            bool
 	NewPublicRegisters  int
@@ -23,7 +29,7 @@ type pageData struct {
 	NumUsers            int
 	NumCardsTotal       int
 	NumRegistersTotal   int
-	User
+	Categories          []Categorie
 }
 
 type controllerFunction func(*http.Request, *pageData)
@@ -46,6 +52,82 @@ func HandleWithContext(controllerFunc controllerFunction) func(http.ResponseWrit
 		data.User.MemberSince = "24.12.2018"
 		data.User.NumCards = 24
 		data.User.NumRegisters = 7
+
+		// nature categorie fake
+		nature := Categorie{}
+		nature.Name = "Naturwissenschaften"
+		nature.SubCategories = []string{
+			"Biologie",
+			"Chemie",
+			"Elektrotechnik",
+			"Informatik",
+			"Mathematik",
+			"Medizin",
+			"Naturkunde",
+			"Physik",
+			"Sonstiges",
+		}
+
+		// languages categorie fake
+		languages := Categorie{}
+		languages.Name = "Sprachen"
+		languages.SubCategories = []string{
+			"Chinesisch",
+			"Deutsch",
+			"Englisch",
+			"Fransösisch",
+			"Griechisch",
+			"Italienisch",
+			"Latein",
+			"Russisch",
+			"Sonstiges",
+		}
+
+		// languages categorie fake
+		society := Categorie{}
+		society.Name = "Gesellschaft"
+		society.SubCategories = []string{
+			"Ethik",
+			"Geschichte",
+			"Literatur",
+			"Musik",
+			"Politik",
+			"Recht",
+			"Soziales",
+			"Sport",
+			"Verkehrskunde",
+			"Sonstiges",
+		}
+
+		economy := Categorie{}
+		economy.Name = "Wirtschaft"
+		economy.SubCategories = []string{
+			"BWL",
+			"Finanzen",
+			"Landwirtschaft",
+			"Marketing",
+			"VWL",
+			"Sonstiges",
+		}
+
+		mindstuff := Categorie{}
+		mindstuff.Name = "Geisteswissenschaften"
+		mindstuff.SubCategories = []string{
+			"Kriminalogie",
+			"Philosophie",
+			"Psychologie",
+			"Pädagogik",
+			"Theologie",
+			"Sonstiges",
+		}
+
+		data.Categories = []Categorie{
+			nature,
+			languages,
+			society,
+			economy,
+			mindstuff,
+		}
 
 		controllerFunc(r, &data)
 
