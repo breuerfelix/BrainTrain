d09c5b518591efcbc48310f2aee702470f25dbd1
Silvan Büdenbender <s.buedenbender@gipfelstuermer-kommunikation.com>
2019-05-17 21:36:33 +0200

    WIP: Make privateRegisters dynamic

--- app/controller/privateRegisters.go
+++ app/controller/privateRegisters.go
@@ -1,8 +1,47 @@
 package controller
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/breuerfelix/BrainTrain/app/models"
+)
 
 // PrivateRegisters controller
 func PrivateRegisters(r *http.Request, data *GeneralData, pageData *PageData) {
 	data.Filename = "private-registers"
+
+	allPrivateRegisters, err := models.DB.QueryJSON(fmt.Sprintf(`{
+		"selector": {
+			"type": {
+				"$eq": "register"
+			},
+			"user": {
+				"$eq": "%s"
+			}
+		}
+	}`, data.Username))
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, register := range allPrivateRegisters {
+		allCards, _ := models.DB.QueryJSON(fmt.Sprintf(`{
+			"selector": {
+				"type": {
+					"$eq": "card"
+				},
+				"register_id": {
+					"$eq": "%s"
+				}
+			}
+		}`, register["_id"]))
+
+		register["amountCards"] = len(allCards)
+	}
+
+	println(len(allPrivateRegisters))
+
+	(*pageData)["registers"] = &allPrivateRegisters
 }
--- app/controller/profile.go
+++ app/controller/profile.go
@@ -2,7 +2,17 @@ package controller
 
 import "net/http"
 
+type userStatistics struct {
+	Cards     int
+	Registers int
+}
+
 // Profile controller
 func Profile(r *http.Request, data *GeneralData, pageData *PageData) {
 	data.Filename = "profile"
+
+	(*pageData)["stats"] = &userStatistics{
+		Cards:     49,
+		Registers: 9,
+	}
 }
